1422d9766842229b5a223cdd18f3ef09cad9387a
zakio <infectious3@gmail.com>
2022-06-12 22:01:13 +0900

    ♻️ export defaultの削除

--- components/Headline.js
+++ components/Headline.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Headline(props) {
+export function Headline(props) {
     console.log(props.title);
 
   return (
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Footer } from '../components/Footer'
-import Headline from '../components/Headline'
-import Links from '../components/Links'
+import { Headline } from '../components/Headline'
+import { Links } from '../components/Links'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
